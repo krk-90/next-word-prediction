5587cd694f44142462de52bd414fed8275cedbe4
krk-90 <rajagirikaushik@gmail.com>
2026-07-30 12:35:37 +0530

    update

--- src/model.py
+++ src/model.py
@@ -1,48 +1,29 @@
-import pickle
 import torch
-import torch.nn as nn 
-from pathlib import Path
+import torch.nn as nn
+
 
 class Hybrid_model(nn.Module):
-    def __init__(self,tokens_size,embedded_dim = 64,hidden_dim = 128):
+    def __init__(self, vocab_size,embedded_dim = 64,hidden_dim = 128):
         super(Hybrid_model,self).__init__()
 
-        self.embedding = nn.Embedding(tokens_size,embedded_dim)
+        self.embedding = nn.Embedding(vocab_size,embedded_dim)
         self.lstm = nn.LSTM(embedded_dim,hidden_dim,batch_first=True)
         self.gru = nn.GRU(embedded_dim,hidden_dim,batch_first=True)
 
-        self.fc =nn.Sequential(
+        self.fc = nn.Sequential(
             nn.Linear(hidden_dim*2,64),
             nn.ReLU(),
-            nn.Linear(64,tokens_size)
+            nn.Linear(64,vocab_size)
         )
 
     def forward(self,x):
-        x = self.embedding(x)
-        lstm_output,_ = self.lstm(x)    
-        gru_output,_ = self.gru(x)
-        lstm_last = lstm_output[:,-1,:]
-        gru_last = gru_output[:,-1,:]
-        combined = torch.cat((lstm_last,gru_last),dim=-1)
-        return self.fc(combined)
+        x = self.embedding(x) 
+        lstm_out,_ = self.lstm(x)
+        gru_out,_ = self.gru(x)   
+        lstm_last = lstm_out[:,-1,:]
+        gru_last = gru_out[:,-1,:]
+        x = torch.cat((lstm_last, gru_last), dim=-1)
+        x = self.fc(x)
+        return x
 
-def model():
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    file_path = Path(__file__).resolve().parent.parent
-    tokens_path = file_path/"Data"/"tokens.pkl"
-    params_path = file_path/"Data"/"params.pth"
-    if not (tokens_path.exists() and params_path.exists()):
-        raise FileNotFoundError(f"Missing {tokens_path} or {params_path}")
-
-    with open(tokens_path,"rb") as f:
-        tokens = pickle.load(f)
-
-    parameters = torch.load(params_path,map_location=device,weights_only=True)    
-
-    hybrid_moded = Hybrid_model(tokens_size=len(tokens))
-    hybrid_moded.load_state_dict(parameters["model_state_dict"])
-    hybrid_moded.to(device)
-    hybrid_moded.eval()
-    return hybrid_moded
-if __name__ == "__main__":
-    MODEL = model()
+    
