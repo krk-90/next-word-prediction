2db5e6bea2a1eea6fa34920a7dfd84fd551781b4
krk-90 <rajagirikaushik@gmail.com>
2026-07-31 09:26:56 +0530

    modified

--- app/backend/preprocess.py
+++ app/backend/preprocess.py
@@ -2,9 +2,8 @@ from pathlib import Path
 import pickle
 import torch
 import spacy
-from torch.nn.utils.rnn import pad_sequence
 
-class preprocess:
+class Preprocess:
     def __init__(self,vocab,seq_length = 5):
         self.vocab = vocab
         self.seq_length = seq_length
@@ -16,7 +15,7 @@ class preprocess:
         doc = self.nlp(text)
         tokens = [token.text.lower() for token in doc if not token.is_space]
         if len(tokens) < self.seq_length:
-            raise ValueError(f"need at leasst {self.seq_length} words,input has {len(tokens)}")
+            raise ValueError(f"need at least {self.seq_length} words,input has {len(tokens)}")
 
         tokens = tokens[-self.seq_length:]
         return [self.vocab.get(tok,self.unk_idx) for tok in tokens]
@@ -24,21 +23,16 @@ class preprocess:
     def to_tensor(self,seq):
         return torch.tensor([seq],dtype=torch.long)
     
-def main():
+def main(text:str = "Enter a sequence of text."):
     file_path = Path(__file__).resolve().parent.parent
     path = file_path/"Data"/"tokens.pkl"
     with open(path,"rb") as f:
         vocab = pickle.load(f)
 
-    preprocessor = preprocess(vocab=vocab)  
-
-    text =input("enter text:")
-
+    preprocessor = Preprocess(vocab=vocab)  
     seq = preprocessor.text_to_seq(text)
     x = preprocessor.to_tensor(seq)
-    print("Token IDs:", seq)
-    print("Tensor:", x)
-    print("Shape:", x.shape)
     return x
+
 if __name__ == "__main__":
     main()
